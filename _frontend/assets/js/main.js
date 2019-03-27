f575c7e0f98a633d67559045c16383c988f66eb4
wismannur <wismannura@gmail.com>
2019-03-27 11:52:33 +0700

    add new filter for setail task

--- _frontend/assets/js/main.js
+++ _frontend/assets/js/main.js
@@ -3,19 +3,26 @@ $(document).ready(function() {
     window.monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
 
     function myFilterTable() {
-        var input, filter, table, tr, td, i, txtValue;
-        input = document.getElementById("myInput");
+        var input, filter, table, tr, td, i, txtValue, fil;
+        input = document.getElementById("myInputFilter");
         filter = input.value.toUpperCase();
         table = document.getElementById("myTable");
         tr = table.getElementsByTagName("tr");
+        fil = $('#filterTable').val();
+        console.log(fil)
         for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[0];
+            if (fil == "easy" || fil == "difficult") {
+                td = tr[i].getElementsByTagName("td")[3];
+            } else if (fil == "done" || fil == "not done") {
+                td = tr[i].getElementsByTagName("td")[4];
+            }
+
             if (td) {
                 txtValue = td.textContent || td.innerText;
                 if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
+                    tr[i].style.display = "";
                 } else {
-                tr[i].style.display = "none";
+                    tr[i].style.display = "none";
                 }
             }       
         }
@@ -86,6 +93,10 @@ $(document).ready(function() {
 
     $('#forDate').datetimepicker({
         format : 'YYYY-MM-DD',
+    })
+
+    $('#time').datetimepicker({
+        format : 'HH:mm',
     })
 
     // setTimeout(function() {
--- _frontend/assets/js/mytodolist.js
+++ _frontend/assets/js/mytodolist.js
@@ -13,31 +13,30 @@ $(document).ready(function() {
                 console.log(data)
                 if (data != "0 results") {
                     let result = JSON.parse(data)
-                    // for (let i = 0; i < result.length; i++) {
-                    //     let for_date = result[i].for_date.split('-');
-                    //     let convert_for_date = convertDate( for_date[2], for_date[1], for_date[0] )
-    
-                    //     let tr = `<tr class="data-list">
-                    //         <td>${i+1}</td>
-                    //         <td>${result[i].name_todo}</td>
-                    //         <td class="for_date" data-date= "${result[i].for_date}">${convert_for_date}</td>
-                    //         <td>${result[i].description}</td>
-                    //         <td><button id="detailTodo" class="btn btn-primary btn-left" data-id="${result[i].id}" onclick="detailTodo(${i+1})">Detail</button>
-                    //         <button id="editTodo" class="btn btn-warning btn-center" data-id="${result[i].id}" onclick="editTodo(${i+1})">Edit</button>
-                    //         <button id="deleteTodo" class="btn btn-danger btn-right" data-toggle="modal" data-target="#modalDeleteToDo" data-id="${result[i].id}" onclick="deleteTodo(${i+1})">Delete</button></td></tr>`
-                    //     $('#myTable').find('tbody').append(tr)
-                    // }
+                    for (let i = 0; i < result.length; i++) {
+                        let tr = `<tr class="data-list">
+                            <td>${i+1}</td>
+                            <td class="time">${result[i].time}</td>
+                            <td>${result[i].name_todo_item}</td>
+                            <td class="category">${result[i].category}</td>
+                            <td class="status">${result[i].status}</td>
+                            <td><button id="editTodo" class="btn btn-warning btn-left" data-id="${result[i].id}" onclick="editTodo(${i+1})">Edit</button>
+                            <button id="deleteTodo" class="btn btn-danger btn-right" data-toggle="modal" data-target="#modalDeleteToDo" data-id="${result[i].id}" onclick="deleteTodo(${i+1})">Delete</button></td></tr>`
+                        $('#myTable').find('tbody').append(tr)
+                    }
         
                     console.log(result)
                     // $('.spinner_req').hide();
                 } else {
-                    // let tr = `<tr class="data-list">
-                    //     <td></td>
-                    //     <td style="text-align:center;" >No Data ToDoList</td>
-                    //     <td class="for_date">Please</td>
-                    //     <td style="text-align:center;" >Add New ToDoList</td>
-                    //     <td></td></tr>`
-                    // $('#myTable').find('tbody').append(tr)
+                    let tr = `<tr class="data-list">
+                        <td></td>
+                        <td style="text-align:center;" >No Data ToDoList</td>
+                        <td class="for_date">Please</td>
+                        <td style="text-align:center;" >Add New ToDoList</td>
+                        <td></td>
+                        <td></td>
+                        </tr>`
+                    $('#myTable').find('tbody').append(tr)
                 }
                 $('body').addClass('loaded');
             }
